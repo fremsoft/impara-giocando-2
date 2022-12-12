f1b0616dc371ea402adf726868c382e53c3a3efa
fremsoft <41731090+fremsoft@users.noreply.github.com>
2022-12-12 15:54:18 +0100

    Approssimazioni successive

--- 2022-10-17_rollarm/cinematica_inversa/Cinematica.ino
+++ 2022-10-17_rollarm/cinematica_inversa/Cinematica.ino
@@ -27,10 +27,21 @@ void calcoloCinematicoDiretto(int alfa1024, int beta1024, int gamma1024, float *
   zO2mm = zO1mm + 60*sin(alfaRad);
   
   float xPmm, yPmm, zPmm;
+  /* 
+   * SENZA CONSIDERARE I 10 MM DI DISALLINEAMENTO
+   * 
   xPmm = xO2mm + 110*cos(alfaRad - gammaRad)*cos(betaRad);
   yPmm = yO2mm + 110*cos(alfaRad - gammaRad)*sin(betaRad);
   zPmm = zO2mm + 110*sin(alfaRad - gammaRad);
-
+   *
+   */
+  /* 
+   * CONSIDERANDO I 10 MM DI DISALLINEAMENTO IN Z
+   */ 
+  xPmm = xO2mm + (110*cos(alfaRad - gammaRad) - 10*sin(alfaRad - gammaRad))*cos(betaRad);
+  yPmm = yO2mm + (110*cos(alfaRad - gammaRad) - 10*sin(alfaRad - gammaRad))*sin(betaRad);
+  zPmm = zO2mm + (110*sin(alfaRad - gammaRad) + 10*cos(alfaRad - gammaRad));
+   
   *xmm = xPmm;
   *ymm = yPmm;
   *zmm = zPmm;
@@ -44,11 +55,38 @@ void calcoloCinematicoDiretto(int alfa1024, int beta1024, int gamma1024, float *
   */
 }
 
+float calcoloErrore ( float deltaX, float deltaY, float deltaZ ) {
+  
+  return ((deltaX*deltaX) + (deltaY*deltaY) + (deltaZ*deltaZ));
+  
+}
+
+
+void tentativoMovimento(int alfa1024_tmp, int beta1024_tmp, int gamma1024_tmp, 
+                        float *errore1,
+                        float x2mm, float y2mm, float z2mm,
+                        int *alfa1024_ok, int *beta1024_ok, int *gamma1024_ok) {
+                        
+  float xmm_tmp, ymm_tmp, zmm_tmp, errore2;
+ 
+  calcoloCinematicoDiretto(alfa1024_tmp, beta1024_tmp, gamma1024_tmp, &xmm_tmp, &ymm_tmp, &zmm_tmp);
+  errore2 = calcoloErrore ( (x2mm - xmm_tmp), (y2mm - ymm_tmp), (z2mm - zmm_tmp));
+
+  if (errore2 < *errore1) { 
+    *alfa1024_ok = alfa1024_tmp;
+    *beta1024_ok = beta1024_tmp;
+    *gamma1024_ok = gamma1024_tmp;
+
+    *errore1 = errore2;
+  }
+  
+}
+
 void writeServo() {
 
-  Servo_1.write(map(alfa1024, 0, 1023, 0, 180)); 
-  Servo_0.write(map(beta1024, 0, 1023, 0, 180)); 
-  Servo_2.write(map(gamma1024, 0, 1023, 0, 180)); 
+  Servo_1.write(map(alfa1024, 0, 1023, 0, 180)       + OFFSET_ALFA); 
+  Servo_0.write(map(beta1024, 0, 1023, 0, 180)       + OFFSET_BETA); 
+  Servo_2.write(map(gamma1024, 0, 1023, 0, 180)      + OFFSET_GAMMA); 
   Servo_3.write(map(potenziometro[3], 0, 1023, 100, 180)); 
 
 }
--- 2022-10-17_rollarm/cinematica_inversa/Record.ino
+++ 2022-10-17_rollarm/cinematica_inversa/Record.ino
@@ -1,8 +1,8 @@
 //Read the values ot the potentiometers.
 void readPot()
 {
-  potenziometro[0] = analogRead(A0);
-  potenziometro[1] = analogRead(A1);
-  potenziometro[2] = analogRead(A2);
-  potenziometro[3] = analogRead(A3);
+  potenziometro[0] = analogRead( POTENZIOMETRO_1 );
+  potenziometro[1] = analogRead( POTENZIOMETRO_2 );
+  potenziometro[2] = analogRead( POTENZIOMETRO_3 );
+  potenziometro[3] = analogRead( POTENZIOMETRO_4 );
 }
