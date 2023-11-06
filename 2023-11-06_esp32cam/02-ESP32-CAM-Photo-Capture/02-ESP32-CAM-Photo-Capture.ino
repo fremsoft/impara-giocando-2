67331ab428a4e96608954761740c3dcaeb07e34a
fremsoft <41731090+fremsoft@users.noreply.github.com>
2023-11-06 16:00:25 +0100

    prova a 20MHz

--- 2023-11-06_esp32cam/02-ESP32-CAM-Photo-Capture/02-ESP32-CAM-Photo-Capture.ino
+++ 2023-11-06_esp32cam/02-ESP32-CAM-Photo-Capture/02-ESP32-CAM-Photo-Capture.ino
@@ -68,7 +68,7 @@ void setup() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 2000000; //20000000;
+  config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
   if(psramFound()){
